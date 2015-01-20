ee5dd8bf1923476b9a2b84b86e29d3f74becddd1
Roberto Zen <rob.zenn92@hotmail.it>
2015-01-20 18:01:21 +0100

    Facebook requests for token added

---
+++ DBService/src/auth/Settings.java
@@ -0,0 +1,17 @@
+package auth;
+
+public final class Settings {
+	
+	// Common Settings
+	public static final String REQ_TYPE = "GET";
+	public static final String BASE_PROTOCOL = "http://";
+	
+	// ##############
+	// ## SERVICES ##
+	// ##############
+	
+	// Facebook Service
+	public static final String FB_BASE_URL = "localhost:";
+	public static final String FB_BASE_PORT = "8020";
+	public static final String FB_BASE_PATH = "/project-facebook/auth?token=";
+}
